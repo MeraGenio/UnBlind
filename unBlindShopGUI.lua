e38d2acd1b976cde7687bc922354d13635885911
Soap <34732735+MeraGenio@users.noreply.github.com>
2025-04-15 15:15:32 +1000

    Add files via upload

---
+++ unBlindShopGUI.lua
@@ -0,0 +1,11 @@
+-- we gaming!!!
+
+if SMODS.Atlas then
+    SMODS.Atlas({
+        key = "modicon",
+        path = "icon.png",
+        px = 34,
+        py = 34,
+        frames = 20
+    })
+end
